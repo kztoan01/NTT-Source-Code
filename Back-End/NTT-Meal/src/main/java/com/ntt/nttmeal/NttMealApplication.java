1ea736c8e252edfb637f3bbd33ae0e0f8f72bd15
Wiramin <tgnhatminh.forwork@gmail.com>
2024-05-16 18:06:33 +0700

    fix docker-compose elastic, env file and create MealService

---
+++ Back-End/NTT-Meal/src/main/java/com/ntt/nttmeal/NttMealApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.nttmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttMealApplication.class, args);
+	}
+
+}
